886e9e18a3216a407c5a6b4cb2f2f4ae8d91e03a
josteen1luv <sample@gmail.com>
2024-03-21 17:11:11 +0100

    done project

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,29 @@
+import './search-panel.css';
+import {Component} from "react";
+class SearchPanel extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdate = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+    render() {
+        return(
+            <input
+                type="text"
+                className='form-control search-input'
+                placeholder='Find an employee'
+                value={this.state.term}
+                onChange={this.onUpdate}
+            />
+        )
+    }
+}
+
+export default SearchPanel;
